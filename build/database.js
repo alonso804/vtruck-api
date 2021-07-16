ca58cb15abe6bcac9ad852efd5b767699b97b70d
alonso804 <alonsobs804@gmail.com>
2021-07-15 22:19:50 -0500

    update api

--- build/database.js
+++ build/database.js
@@ -4,7 +4,7 @@ var _mongoose = _interopRequireDefault(require("mongoose"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_mongoose.default.connect("".concat(process.env.MONGO_URL_DEV), {
+_mongoose.default.connect(process.env.MONGO_URL_DEV, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log("DB is connected on ".concat(process.env.MONGO_URL_DEV))).catch(err => console.error(err));
